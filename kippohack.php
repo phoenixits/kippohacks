23a02b7912d795d2152f907cd86528d9bd450796
phoenixits <bounty@phoenixit.org>
2015-02-07 19:27:03 +0100

    kippohack.php

---
+++ kippohack.php
@@ -0,0 +1,18 @@
+<?php
+$log = "/kippo/data/lastlog.txt"; // modify this to your system
+$linecount = 0;
+$handle = fopen($log, "r");
+while(!feof($handle)){
+   $line = fgets($handle);
+   $linecount++;
+}
+fclose($handle);
+echo "<b>Hacking Attempts:</b> $linecount<br>";
+echo "<b>Breaches:</b> 0<br>";
+        echo "<b>Last:</b>&nbsp;";
+$log = escapeshellarg($log);
+$line = `tail -n 1 $log`;
+$strip = array("root", "pts/0", $line);
+$line = str_replace($strip, "", $line);
+echo "$line";
+?>
